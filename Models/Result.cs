7186808bd22159943e0758de80a1b29811506b38
Alexander Karlsson <info@alexanderk.se>
2023-02-03 07:42:23 +0100

    Initial commit

---
+++ Models/Result.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System;
+
+namespace SearchengineResult.Models
+{
+    public class Result
+    {
+        public String Name { get; set; }
+        public long ResultCount { get; set; }
+        public String ResultCountConverted
+        {
+            get
+            {
+                if (ResultCount > 999999999 || ResultCount < -999999999)
+                {
+                    return ResultCount.ToString("#,##0,,,B", CultureInfo.InvariantCulture);
+                }
+                else if (ResultCount > 999999 || ResultCount < -999999)
+                {
+                    return ResultCount.ToString("#,##0,,M", CultureInfo.InvariantCulture);
+                }
+                else if (ResultCount > 999 || ResultCount < -999)
+                {
+                    return ResultCount.ToString("#,##0,K", CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    return ResultCount.ToString(CultureInfo.InvariantCulture);
+                }
+            }
+        }
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using SearchengineResult.Models;
+using SerpApi;
+using System.Collections;
+using System.Web;
+using static System.Net.WebRequestMethods;
+
+namespace SearchengineResult.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<Result> Results { get; set; } = new List<Result>();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public async void OnPost(string search)
+        {
+            String[] searchWords = search.Split(' ');
+
+            
+        }
+    }
+}
