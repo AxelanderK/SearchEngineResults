e6d181bf2a1553db5328dc73bd34bb53944985c6
Alexander Karlsson <info@alexanderk.se>
2023-02-04 22:35:41 +0100

    Extension method for List collection

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace SearchengineResult.Extensions
+{
+    public static class Extensions
+    {
+        public static void AddIfNotNull<T>(this List<T> coll, T item)
+        {
+            if (item != null)
+            {
+                coll.Add(item);
+            }
+        }
+    }
+}
